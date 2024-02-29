916cf672fbfc96e8f3e24cff6a466d4b12989797
Marco Moauro <marco.moauro@translated.net>
2024-02-29 11:36:05 +0100

    create new api

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import {healthcheck} from "./api/healthcheck.js";
 import {routeToFunction} from "./middlewares.js";
 import {throw422, throw500} from "./controllers/errors.js";
 import {getNewsletter} from "./controllers/newsletters.js";
+import {pong} from "./api/pong.js";
 
 const router = new Router();
 
@@ -12,5 +13,7 @@ router.get('/errors/422', routeToFunction(throw422));
 router.get('/errors/500', routeToFunction(throw500));
 
 router.get('/newsletters/:id', routeToFunction(getNewsletter));
+
+router.get('/ping', routeToFunction(pong));
 
 export default router;
